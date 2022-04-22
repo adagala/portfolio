daa2cb55eb35305c7670131bb3ab20f52f8de932
Henry Adagala <adagalahenry@gmail.com>
2022-04-22 09:55:42 +0300

    updated tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  purge: [
+  content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
