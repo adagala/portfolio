e7e5a5e5a81f61ce5a03e02a1485bc408cf7a3a8
Henry Adagala <adagalahenry@gmail.com>
2022-04-29 10:51:13 +0300

    close job advert

--- pages/software-developer.tsx
+++ pages/software-developer.tsx
@@ -17,114 +17,121 @@ const SoftwareDeveloper: NextPage = () => {
       </Head>
 
       <div className="flex justify-center m-5">
+        <div className="absolute flex justify-center items-center h-full">
+          <div className="text-5xl text-red-500 uppercase border-8 border-red-500 p-8 font-bold origin-bottom -rotate-12">
+            Closed
+          </div>
+        </div>
         <div className="">
           <div className="text-center font-bold text-3xl underline mt-3 font-serif">
             FULL STACK DEVELOPER
           </div>
-          <div>
-            <div className="font-semibold text-xl uppercase mt-3 font-serif">
-              Project
-            </div>
-            <div>Performance Management / Human Resource Management</div>
-          </div>
-          <div>
-            <div className="font-semibold text-xl uppercase mt-3 font-serif">
-              Timeline
-            </div>
-            <div>3 months (1st May 2022 - 31st July 2022)</div>
-          </div>
-          <div>
-            <div className="font-semibold text-xl uppercase mt-3 font-serif">
-              Qualities
-            </div>
+          <div className="opacity-20">
             <div>
-              <ul className="list-disc ml-4">
-                <li>A team player</li>
-                <li>A good communicator</li>
-                <li>Good understanding of Front End Enterprise Design</li>
-                <li>
-                  A Human Resources/Performance Management work background would
-                  be an added bonus
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div>
-            <div className="font-semibold text-xl uppercase mt-3 font-serif">
-              Skills
-            </div>
-            <div className="flex">
-              <div className="w-1/2">
-                <ul className="list-disc ml-4">
-                  <li>Angular</li>
-                  <li>NgRx</li>
-                  <li>Tailwind</li>
-                </ul>
+              <div className="font-semibold text-xl uppercase mt-3 font-serif">
+                Project
               </div>
-              <div className="w-1/2">
-                <ul className="list-disc ml-4">
-                  <li>Firebase</li>
-                  <li>Git</li>
-                  <li>Node.js</li>
-                </ul>
+              <div>Performance Management / Human Resource Management</div>
+            </div>
+            <div>
+              <div className="font-semibold text-xl uppercase mt-3 font-serif">
+                Timeline
               </div>
-            </div>
-          </div>
-          <div>
-            <div className="font-semibold text-xl uppercase mt-3 font-serif">
-              More Info
+              <div>3 months (1st May 2022 - 31st July 2022)</div>
             </div>
             <div>
-              <ul className="list-disc ml-4">
-                <li>100% Remote</li>
-                <li>Flexible working hours</li>
-              </ul>
-            </div>
-          </div>
-          <div>
-            <div className="font-semibold text-xl uppercase mt-3 font-serif">
-              Compensation
-            </div>
-            <div>
-              KES 50,000/month (Subject to tasks allocation & completion)
-            </div>
-          </div>
-          <div>
-            <div className="font-semibold text-xl uppercase mt-3 font-serif">
-              Deadline
-            </div>
-            <div>
-              April 28<sup>th</sup>, 2022
-            </div>
-          </div>
-          <div>
-            <div className="font-semibold text-xl uppercase mt-3 font-serif">
-              Communication
-            </div>
-            <div>
-              <a href="mailto:adagalahenry@gmail.com">
-                <div className="flex items-center hover:opacity-50">
-                  <MailIcon
-                    className="flex-shrink-0 h-5 w-5"
-                    aria-hidden="true"
-                  ></MailIcon>
-                  <span className="ml-2">adagalahenry@gmail.com</span>
-                </div>
-              </a>
+              <div className="font-semibold text-xl uppercase mt-3 font-serif">
+                Qualities
+              </div>
               <div>
-                <a
-                  href="https://twitter.com/messages/compose?recipient_id=1044264361"
-                  target="_blank"
-                  className="flex items-center hover:opacity-50"
-                >
-                  <ChatAltIcon
-                    className="flex-shrink-0 h-5 w-5"
-                    aria-hidden="true"
-                  ></ChatAltIcon>
-                  <span className="ml-2">
-                    twitter.com/adagalahenry (Direct Message Only)
-                  </span>
+                <ul className="list-disc ml-4">
+                  <li>A team player</li>
+                  <li>A good communicator</li>
+                  <li>Good understanding of Front End Enterprise Design</li>
+                  <li>
+                    A Human Resources/Performance Management work background
+                    would be an added bonus
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div>
+              <div className="font-semibold text-xl uppercase mt-3 font-serif">
+                Skills
+              </div>
+              <div className="flex">
+                <div className="w-1/2">
+                  <ul className="list-disc ml-4">
+                    <li>Angular</li>
+                    <li>NgRx</li>
+                    <li>Tailwind</li>
+                  </ul>
+                </div>
+                <div className="w-1/2">
+                  <ul className="list-disc ml-4">
+                    <li>Firebase</li>
+                    <li>Git</li>
+                    <li>Node.js</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="font-semibold text-xl uppercase mt-3 font-serif">
+                More Info
+              </div>
+              <div>
+                <ul className="list-disc ml-4">
+                  <li>100% Remote</li>
+                  <li>Flexible working hours</li>
+                </ul>
+              </div>
+            </div>
+            <div>
+              <div className="font-semibold text-xl uppercase mt-3 font-serif">
+                Compensation
+              </div>
+              <div>
+                KES 50,000/month (Subject to tasks allocation & completion)
+              </div>
+            </div>
+            <div>
+              <div className="font-semibold text-xl uppercase mt-3 font-serif">
+                Deadline
+              </div>
+              <div>
+                April 28<sup>th</sup>, 2022
+              </div>
+            </div>
+            <div>
+              <div className="font-semibold text-xl uppercase mt-3 font-serif">
+                Communication
+              </div>
+              <div className="hidden">
+                <a href="mailto:adagalahenry@gmail.com">
+                  <div className="flex items-center hover:opacity-50">
+                    <MailIcon
+                      className="flex-shrink-0 h-5 w-5"
+                      aria-hidden="true"
+                    ></MailIcon>
+                    <span className="ml-2">adagalahenry@gmail.com</span>
+                  </div>
                 </a>
+                <div>
+                  <a
+                    href="https://twitter.com/messages/compose?recipient_id=1044264361"
+                    target="_blank"
+                    className="flex items-center hover:opacity-50"
+                  >
+                    <ChatAltIcon
+                      className="flex-shrink-0 h-5 w-5"
+                      aria-hidden="true"
+                    ></ChatAltIcon>
+                    <span className="ml-2">
+                      twitter.com/adagalahenry (Direct Message Only)
+                    </span>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
