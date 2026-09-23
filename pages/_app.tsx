5decab108f14dbfe27b65810f26cb03b24495612
Henry Adagala <adagalahenry@gmail.com>
2026-09-23 17:36:31 +0300

    feat: enhance favicon and add apple touch icon for improved branding

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,9 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="48x48" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
       <Component {...pageProps} />
     </>
